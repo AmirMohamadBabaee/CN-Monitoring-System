2180ebe1f502678f509f8b5cf65c047844a768c2
amir01 <iran.volleyball79@gmail.com>
2022-06-08 16:15:30 +0430

    add prometheus_client metrics and expose them

--- src/metric_server.py
+++ src/metric_server.py
@@ -1,9 +1,9 @@
-import sys
 import socket
 import types
 import selectors
 import logging
 import json
+from prometheus_client import Counter, Gauge, Histogram, start_http_server
 
 HOST = '127.0.0.1'
 PORT = 9999
@@ -17,6 +17,7 @@ class MetricServer:
         self.sel = selectors.DefaultSelector()
         self.logger = self.init_logger()
         self.main_socket = self.init_socket()
+        self.prometues_metrics = self.init_prometheus_metrics()
 
     def init_logger(self):
         # init logger object
@@ -45,7 +46,9 @@ class MetricServer:
         return lsock
 
     def run(self):
+        
         try:
+            start_http_server(8000)
             while True:
                 events = self.sel.select(timeout=None)
                 for key, mask in events:
@@ -108,8 +111,52 @@ class MetricServer:
         
         metrics_json = metrics_json_encoded.decode('utf-8')
         metrics = json.loads(metrics_json)
-        print(f'metrics: {metrics}')
-        return metrics
+        # print(f'metrics: {metrics}')
+        self.update_prometheus_metrics(metrics)
+
+    def init_prometheus_metrics(self):
+
+        prometheus_metrics = dict()
+
+        prometheus_metrics['cpu_utilization_percent']  = Gauge('cpu_utilization_percent', 'current CPU utilization', ['agent_name'])
+        prometheus_metrics['cpu_frequency_average']    = Histogram('cpu_frequency_average', 'current average of CPU frequency of all cores', ['agent_name'])
+        prometheus_metrics['cpu_temperature']          = Histogram('cpu_temperature', 'current temperature of acpitz (CPU)', ['agent_name'])
+        prometheus_metrics['cpu_fan_speed']            = Histogram('cpu_fan_speed', 'current CPU fan speed in RPM', ['agent_name'])
+
+        prometheus_metrics['memory_usage_percent']     = Gauge('memory_usage_percent', 'current memory usage', ['agent_name'])
+        prometheus_metrics['memory_usage_bytes']       = Histogram('memory_usage_bytes', 'current used memory bytes', ['agent_name'])
+
+        prometheus_metrics['swap_usage_percent']       = Gauge('swap_usage_percent', 'current swap usage', ['agent_name'])
+        prometheus_metrics['swap_usage_bytes']         = Histogram('swap_usage_bytes', 'current used swap bytes', ['agent_name'])
+
+        prometheus_metrics['net_packet_sent_count']    = Gauge('net_packet_sent', 'current number of sent packets', ['agent_name'])
+        prometheus_metrics['net_packet_rcvd_count']    = Gauge('net_packet_rcvd', 'current number of received packets', ['agent_name'])
+        prometheus_metrics['net_connections_number']   = Gauge('net_connections', 'current number of net connections of inet protocol', ['agent_name'])
+
+        prometheus_metrics['battery_percent']          = Gauge('battery_percent', 'current percentage of battery power', ['agent_name'])
+
+        return prometheus_metrics
+
+    def update_prometheus_metrics(self, metrics):
+
+        agent_name = metrics['agent_name']
+
+        self.prometues_metrics['cpu_utilization_percent'].labels(agent_name).set(metrics['cpu_utilization_percent'])
+        self.prometues_metrics['cpu_frequency_average'].labels(agent_name).observe(metrics['cpu_frequency_average'])
+        self.prometues_metrics['cpu_temperature'].labels(agent_name).observe(metrics['cpu_temperature'])
+        self.prometues_metrics['cpu_fan_speed'].labels(agent_name).observe(metrics['cpu_fan_speed'])
+
+        self.prometues_metrics['memory_usage_percent'].labels(agent_name).set(metrics['memory_usage_percent'])
+        self.prometues_metrics['memory_usage_bytes'].labels(agent_name).observe(metrics['memory_usage_bytes'])
+
+        self.prometues_metrics['swap_usage_percent'].labels(agent_name).set(metrics['swap_usage_percent'])
+        self.prometues_metrics['swap_usage_bytes'].labels(agent_name).observe(metrics['swap_usage_bytes'])
+
+        self.prometues_metrics['net_packet_sent_count'].labels(agent_name).set(metrics['net_packet_sent_count'])
+        self.prometues_metrics['net_packet_rcvd_count'].labels(agent_name).set(metrics['net_packet_rcvd_count'])
+        self.prometues_metrics['net_connections_number'].labels(agent_name).set(metrics['net_connections_number'])
+        
+        self.prometues_metrics['battery_percent'].labels(agent_name).set(metrics['battery_percent'])
 
 
 if __name__ == "__main__":
